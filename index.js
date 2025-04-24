f8a39caa4efdba7eff9e3d7cda1244e2fa519402
Glitch (malachite-abrasive-tornado) <none>
2025-04-24 22:21:40 +0000

    🗳️♣️ Updated with Glitch

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/:date?",(req,res)=>{
   let date = req.params.date;
   let isValid = !isNaN(date)
-console.log('log: '+ isValid)
+console.log('log: '+ isValid+' '+date)
   if(!date)
   {
     let now = new Date()
@@ -40,7 +40,6 @@ console.log('log: '+ isValid)
   {
   var utcFlag = checkDate(date)
   if(utcFlag){
-    console.log('log: '+ isValid)
     res.json({utc: new Date(parseInt(date)).toUTCString()})
   }
   else{
